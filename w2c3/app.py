2170ce335bcd898761f6b4c27723fc62102ddc78
ernie14674560 <ernie14674560@gmail.com>
2018-06-10 21:47:25 +0800

    增加上課註解

--- w2c3/app.py
+++ w2c3/app.py
@@ -3,8 +3,8 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from pymongo import MongoClient
+from sqlalchemy import event
 from datetime import datetime
-
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -12,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/shiyanlou'
 db = SQLAlchemy(app)
 client = MongoClient('127.0.0.1', 27017)
 mdb = client.shiyanlou
+
 
 class File(db.Model):
     __tablename__ = 'files'
@@ -21,7 +22,7 @@ class File(db.Model):
     created_time = db.Column(db.DateTime)
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
     content = db.Column(db.Text)
-    category = db.relationship('Category', uselist=False)
+    category = db.relationship('Category', uselist=False)  # 獲取文章的分類 uselist false代表不返回列表
 
     def __init__(self, title, created_time, category, content):
         self.title = title
@@ -31,10 +32,13 @@ class File(db.Model):
 
     # 向文章添加標籤
     def add_tag(self, tag_name):
+        mdb.file.update_one({'_id': self.id}, {'$addToSet': {'tags': tag_name}})  # 加到集合addtoset
+
+    '''def add_tag(self, tag_name):
         # 為當前文章添加 tag_name 標籤存入到 MongoDB
-        tag = {'id': self.id, 'tag': tag_name}
+        tag = {'id': self.id, 'tag': tag_name}，# addToSet高級指令? pull?
         mdb.tag.insert_one(tag)
-        pass
+        pass'''
 
     # 移除標籤
     def remove_tag(self, tag_name):
@@ -44,21 +48,41 @@ class File(db.Model):
 
     # 標籤列表
     @property
-    def tags(self):
+    def __file(self):
+        return mdb.file.find_one({'_id': self.id})
+
+    '''def tags(self):
         # 讀取 mongodb，返回當前文章的標籤列表
         result = []
         cursor = mdb.tag.find({'id': self.id})
         for n in cursor:
             result.append(n['tag'])
-        return result
+        return result'''
 
+    @property
+    def tags(self):
+        return self.__file['tags']
+
+
+# File 對象插入數據庫時， 自動創建關聯的mdb對象
+@event.listens_for(File, 'after_insert')  # sql監聽到 after insert 後，執行auto_create_mongodb_file
+def auto_create_mongodb_file(mapper, conn, file):
+    mdb.file.insert_one({'_id': file.id})
+
+
+@event.listens_for(File, 'after_delete')  # sql監聽到 after delete 後，執行auto_delete_mongodb_file
+def auto_delete_mongodb_file(mapper, conn, file):
+    mdb.file.delete_one({'_id': file.id})
 
 
 class Category(db.Model):
     __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    files = db.relationship('File')
+    files = db.relationship('File')  # 可加參數 lazy='dyanmic'，返回query object 後可進行.filter_by(),.all()等動作
+                                     # lazy='select'，訪問到屬性的時候，就會全部加載該屬性的數據
+                                     # lazy='joined，對關聯的兩個表進行join操作，從而獲取到所有相關的對象
+
     def __init__(self, name):
         self.name = name
 
@@ -69,13 +93,13 @@ def index():
     return render_template('index.html', files=files)
 
 
-@app.route('/files/<file_id>')
+@app.route('/files/<int:file_id>')
 def file(file_id):
     contents = File.query.get_or_404(file_id)
     return render_template('file.html', contents=contents)
 
 
-@app.errorhandler(404)
+@app.errorhandler(404)  # 如果errorhandler為404則執行 not_found
 def not_found(error):
     return render_template('404.html'), 404
 
